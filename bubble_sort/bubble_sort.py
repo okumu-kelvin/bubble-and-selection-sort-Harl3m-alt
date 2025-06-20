4336b45e70e7d602dc342bf821edff3a366afb32
Brian Kipkoech <brian.bwogo@strathmore.edu>
2025-06-20 13:09:03 +0300

    Assignment

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,20 @@
-def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+def bubble_sort(data):
+        n = len(data)
+        for i in range(n):
+        
+            for j in range(0, n - i - 1):
+            
+                if data[j] > data[j + 1]:
+                    data[j], data[j + 1] = data[j + 1], data[j]
+        return data
+data= [5, 3, 2, 4, 4, 1]
+sorted_array = bubble_sort(data)
+print("Sorted array is:", sorted_array)
+
+# reverse the array
+def reverse_array(arr):
+    return arr[::-1]
+
+arr = [1, 2, 3, 4, 5]
+reversed_array = reverse_array(arr)
+print("Reversed array is:", reversed_array)
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,21 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    data = arr.copy() 
+    n = len(data)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if data[j] < data[min_index]:
+                min_index = j
+        data[i], data[min_index] = data[min_index], data[i]
+    return data
+arr = [4, 3, 5, 1, 2]
+sorted_array = selection_sort(arr)
+print("Sorted array is:", sorted_array)
+
+# reverse the array
+def reverse_array(arr):
+    return arr[::-1]
+
+arr = [1, 2, 3, 4, 5]
+reversed_array = reverse_array(arr)
+print("Reversed array is:", reversed_array)
